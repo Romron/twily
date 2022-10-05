4076fa4a55a7f35f984e168751bb1719941b88a9
Romron <Romron@ukr.net>
2022-10-05 16:25:31 +0300

    что то

--- gulp/src/scripts/script.js
+++ gulp/src/scripts/script.js
@@ -3,8 +3,21 @@ window.onload = function () {
 
    document.querySelector('#parser-start-button').onclick = function () {
       ajaxGet(url, function (data) {
-         InitBlock(data);
-         ParseData(data);
+         // InitBlock(data, 'initBlock_1');
+         const initBlock_1 = new InitBlocks('#initBlock_1', data);
+         const initBlock_2 = new InitBlocks('#initBlock_2', '**************');
+
+         console.log(initBlock_1);
+         console.log(initBlock_2);
+
+         let a = initBlock_1.CreateBlock('#initial-data');
+         let b = initBlock_2.CreateBlock('#initial-data');
+
+
+
+         // InitBlock('#initBlock_1', '#initial-data', data);
+         // InitBlock('#initBlock_2', '#initial-data', '**************');
+         PreparationData(data);
       });
    }
 }
@@ -27,19 +40,48 @@ function ajaxGet(url, callbackfunction) {
 
 }
 
-function InitBlock(data) {
-   let initBlock_1 = document.getElementById('initBlock_1');
-   if (!initBlock_1) {
-      initBlock_1 = document.createElement('div');
-      initBlock_1.id = 'initBlock_1';
-      initBlock_1.classList.add('initBlocks');
-      initBlock_1.innerHTML = data;
-      document.querySelector('#initial-data').innerHTML = '';
-      document.querySelector('#initial-data').style.justifyContent = 'space-between';
-      document.querySelector('#initial-data').append(initBlock_1);
+class InitBlocks {
+   constructor(idNewBlock, data) {
+      this.idNewBlock = idNewBlock;
+      // this.idTargetBlock = idTargetBlock;
+      this.data = data;
    }
+
+   CreateBlock(idTargetBlock) {
+      // this.NewBlock = document.getElementById(idNewBlock);
+      if (!document.getElementById(this.idNewBlock)) {
+         this.NewBlock = document.createElement('div');
+         this.NewBlock.id = this.idNewBlock;
+         this.NewBlock.classList.add('initBlocks');
+         this.NewBlock.innerHTML = this.data;
+         document.querySelector(idTargetBlock).innerHTML = '';
+         document.querySelector(idTargetBlock).style.justifyContent = 'space-between';
+         document.querySelector(idTargetBlock).append(this.NewBlock);
+      }
+   }
+
+
 }
 
-function ParseData(data) {
+
+// function InitBlock(idNewBlock, idTargetBlock, data) {
+//    this.NewBlock = document.getElementById(idNewBlock);
+//    this.idTargetBlock = idTargetBlock;
+//    this.data = data;
+//    // console.log(idTargetBlock);
+
+//    if (!this.NewBlock) {
+//       this.NewBlock = document.createElement('div');
+//       console.log(this.NewBlock);
+//       this.NewBlock.id = this.idNewBlock;
+//       this.NewBlock.classList.add('initBlocks');
+//       this.NewBlock.innerHTML = this.data;
+//       document.querySelector(idTargetBlock).innerHTML = '';
+//       document.querySelector(idTargetBlock).style.justifyContent = 'space-between';
+//       document.querySelector(idTargetBlock).append(this.NewBlock);
+//    }
+// }
+
+function PreparationData(data) {
 
 }
