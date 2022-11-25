f3237c8b987116638143f002c6561bb27171ca62
Romron <Romron@ukr.net>
2022-11-25 10:37:15 +0200

    натупил при переносе в ветку

--- gulp/src/scripts/Layer.js
+++ gulp/src/scripts/Layer.js
@@ -1,32 +1,33 @@
 export class Layer {
 
-   constructor(params) {
-      this.HEIGHT_DPI = params.canvasHeight * 2;      // для плавности у величиваю количество точек холста в двое
-      this.WIDTH_DPI = params.canvasWidht * 2;
-      const container = document.getElementById(params.idContainer);
-
+   constructor(container) {
       this.canvas = document.createElement("canvas");
       this.context = this.canvas.getContext('2d');
-      this.canvas.id = params.idCanvas;
       container.appendChild(this.canvas);
 
-      this.canvas.style.height = params.canvasHeight + 'px';
-      this.canvas.style.width = params.canvasWidht + 'px';
-      this.canvas.height = this.HEIGHT_DPI;
-      this.canvas.width = this.WIDTH_DPI;
+      this.canvas.style.height = 600;
+      this.canvas.style.width = 1200;
+
+      this.canvas.height = 1200;
+      this.canvas.width = 2400;
+
+
+      console.log("*this.canvas = ", this.canvas);
+
+
+
 
       // this.FitContainer();
       // addEventListener('resize', () => this.FitContainer(this.canvas));
 
    }
 
-   FitContainer(canv) {
-      /**
-       * подгонка размеров канваса при изменении размеров окна
-       * https://youtu.be/XgXGXEqQJqI
-       */
-      canv.height = canv.offsetWidth;
-      canv.width = canv.offsetHeight;
+   FitContainer() {
+      this.canvas.style.height = 600;
+      this.canvas.style.width = 1200;
+      this.canvas.height = 1200;
+      this.canvas.width = 2400;
 
+      console.log("**this.canvas = ", this.canvas);
    }
 }
--- gulp/src/scripts/Loop.js
+++ gulp/src/scripts/Loop.js
@@ -1,12 +1,19 @@
 export class Loop {
+   // <<<<<<< Updated upstream
    constructor(_update, _display) {
       this.update = _update;
       this.display = _display;
+      // =======
+      //    constructor(update, display) {
+      //       this.update = update;
+      //       this.display = display;
+      // >>>>>>> Stashed changes
 
       this.deltaTime = 0;
       this.lastUpdate = 0;
       this.maxInterval = 40;
 
+      // <<<<<<< Updated upstream
       const proxy = new Proxy({}, {
          set(...args) {
             const result = Reflect.set(...args);
@@ -23,4 +30,20 @@ export class Loop {
 
    }
 
+   // =======
+   //       requestAnimationFrame(stampTime => this.animate(stampTime));
+   //    }
+   animate(currentTime) {
+      requestAnimationFrame(stampTime => this.animate(stampTime));
+
+      this.deltaTime = currentTime - this.lastUpdate;
+
+      if (this.deltaTime < this.maxInterval) {
+         this.update(this.deltaTime / 1000);
+         this.display();
+      }
+
+      this.lastUpdate = currentTime;
+   }
+   // >>>>>>> Stashed changes
 }
