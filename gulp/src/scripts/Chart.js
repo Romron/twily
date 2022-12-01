66a9939bb809150bdc3a0a3127e66ff322ba5b83
Romron <Romron@ukr.net>
2022-12-01 14:20:56 +0200

    шкалы вынесены на отдельные слои

--- gulp/src/scripts/Chart.js
+++ gulp/src/scripts/Chart.js
@@ -6,7 +6,6 @@ import { X_axis, Y_axis } from "./Axis.js";
 export class Chart {
    /**
     * патерн Фасад
-    * собирает весь холст внутри этого класса
     * 
     * необходимые сущьности
     *    холст canv()
@@ -94,8 +93,14 @@ export class Chart {
       this.HEIGHT_GRAPH_FILD = this.HEIGHT_DPI - (this.paddingTop + this.paddingBottom + this.hightXaxis);
       this.WIDTH_GRAPH_FILD = this.WIDTH_DPI - (this.paddingLeft + this.paddingRight + this.widthYaxis);
 
+      canvas.canvas.style.cssText = `position: absolute;
+                                     top:0px;
+                                     left:0px;
+                                     z-index: 5;
+                                     /*border: 1px solid green;*/`;
+
       this.oldMousePosX = 0;  // для отслеживания движения курсора 
-      this.oldMousePosY = 0;  // для отслеживания движения курсора 
+      this.oldMousePosY = 0;  // для отслеживания движения курсора
 
       this.Xaxis = new X_axis(this);      // здесь для того что бы можно было отключать координатную сетку а шкалы оставались
       this.Yaxis = new Y_axis(this);
@@ -204,6 +209,90 @@ export class Chart {
       this.Xaxis.drawAxis(this.data);
       this.Yaxis.drawAxis();
 
+      // координатная сетка по оси X
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = '#ADB5D9';
+      this.ctx.font = '20px Arial';
+
+      this.widthToRight = this.WIDTH_GRAPH_FILD - this.coordinates.xOffset;     // расстояние от нуля графика до края
+      this.widthToLeft = this.coordinates.xOffset;     // расстояние от нуля графика до края
+      this.xLine = 0;
+
+      let nLine = 0;
+      let arrDays = Object.keys(this.data);
+      if (arrDays.length != 0) {
+         for (let n = 0; n < arrDays.length; n++) {
+            if (arrDays[n].endsWith('01')) {
+               this.distanceBetweenLines = this.xLineOld - this.xLine;
+               nLine++;
+               this.xLineOld = this.xLine;
+               this.xLine = Math.round(this.coordinates.xNull - n * this.scaleX - this.coordinates.xOffset);
+               this.distanceBetweenLines = this.xLineOld - this.xLine;
+               if (this.xLine < this.coordinates.xNull && this.xLine > this.paddingLeft) {    // запрет отрисовки координамтной сетки на шкале
+                  this._drawLines(nLine);
+                  this._writeText(arrDays[n], n, nLine);
+               }
+               if (this.distanceBetweenLines < 150 && nLine % 2 != 0) {
+                  continue;
+               }
+            }
+            if (this.xLine < 0) {
+               break;
+            }
+         }
+      } else {
+         console.log(" -- Данные ещё не получены -- ");
+      }
+      this.ctx.stroke();
+
+
+      // координатная сетка по оси Y
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 0.;
+      this.ctx.strokeStyle = '#ADB5D9';
+      this.ctx.font = '20px Arial';
+      let x, y;
+      for (let i = 0; i < this.HEIGHT_DPI + Math.abs(this.coordinates.yOffset); i = i + 10) {
+         x = this.coordinates.xNull;
+         y = this.coordinates.yNull - this.coordinates.yOffset - i * this.scaleY;
+         if (y < this.coordinates.yNull && y > this.paddingTop + 10) {
+            this.ctx.moveTo(x + 10, y);   // 10 -- декоративная риска на оси Y 
+            this.ctx.lineTo(this.paddingLeft, y);
+            this.ctx.strokeText(i * 100, x + 25, y);
+         }
+      }
+      if (this.coordinates.yOffset > 10) {
+         for (let i = 0; i < this.coordinates.yOffset; i = i + 10) {
+            this.ctx.moveTo(this.coordinates.xNull, this.coordinates.yNull - this.coordinates.yOffset + i * this.scaleY);
+            this.ctx.lineTo(this.paddingLeft, this.coordinates.yNull - this.coordinates.yOffset + i * this.scaleY);
+         }
+      }
+      this.ctx.stroke();
+   }
+
+   _drawLines(nLine) {
+      if (nLine == 2) {
+         for (let xLineTR = this.xLine; xLineTR < this.WIDTH_GRAPH_FILD; xLineTR = xLineTR + this.distanceBetweenLines) {
+            this.ctx.moveTo(xLineTR, this.paddingTop + 20);
+            this.ctx.lineTo(xLineTR, this.coordinates.yNull + 20);     // 20 -- декоративная риска на оси Х
+         }
+      }
+      this.ctx.moveTo(this.xLine, this.paddingTop + 20);
+      this.ctx.lineTo(this.xLine, this.coordinates.yNull + 20);    // 20 -- декоративная риска на оси Х
+   }
+
+   _writeText(key) {
+
+      let str = '';
+      let str_1 = '';
+      let str_2 = '';
+      let str_3 = '';
+
+      str_1 = key.slice(8, 10);
+      str_2 = key.slice(5, 7);
+      str_3 = key.slice(0, 4).slice(2, 4);
+      str = `${str_1}.${str_2}.${str_3}`;
+      this.ctx.strokeText(str, this.xLine - 40, this.coordinates.yNull + 40);
    }
 
    horizontalPointerText() {
@@ -248,7 +337,6 @@ export class Chart {
       this.ctx.strokeStyle = '#3A3A3C';
       this.ctx.arc(this.mouse.pos.x + this.paddingRight, this.mouse.pos.y + this.paddingTop, 7, 0, Math.PI * 2);
       this.ctx.stroke();
-      // this.ctx.closePath();
    }
 
    clear() {
