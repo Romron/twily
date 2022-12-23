e6ebf30e7a8c570d14d8bc8d98db3cf6b591de68
Romron <Romron@ukr.net>
2022-12-23 16:20:09 +0200

    запустился с прокси

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // devServer: {
-  //   // proxy: 'http://localhost:4000'
-  //   proxy: 'http://web:80'
-  // }
+  devServer: {
+    // proxy: 'http://localhost:4000'
+    proxy: 'http://web:80'
+  }
 })
