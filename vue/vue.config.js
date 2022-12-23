02da9456dd3cd8c904e2564d0be33f3812b61f87
Romron <Romron@ukr.net>
2022-12-23 15:47:03 +0200

    чистый не настроенный vue

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // devServer: {
+  //   // proxy: 'http://localhost:4000'
+  //   proxy: 'http://web:80'
+  // }
 })
